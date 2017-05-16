a714f2086e979e694ff22f81acaf4555623e3c66
Arthanick <Spider-terr@mail.ru>
2017-05-16 15:23:19 +0700

    added player controller and camera movement

---
+++ Assets/Scripts/Camera_move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_move : MonoBehaviour 
+{
+
+	private Camera Cam;
+	[SerializeField]
+	private float speed = 4f;
+	// Use this for initialization
+	void Start () 
+	{
+		Cam =  GetComponent <Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+			Cam.transform.Translate(Vector3.left * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.RightArrow))
+			Cam.transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}
