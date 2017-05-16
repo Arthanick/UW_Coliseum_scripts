9123d043767e5296fcc7056fa51404c40dbc0af8
Arthanick <Spider-terr@mail.ru>
2017-05-16 20:52:36 +0700

    navigation

--- Assets/Scripts/Camera_move.cs
+++ Assets/Scripts/Camera_move.cs
@@ -7,18 +7,22 @@ public class Camera_move : MonoBehaviour
 	private Camera Cam;
 	[SerializeField]
 	private float speed = 4f;
-	// Use this for initialization
+	// Начальная инициализация
 	void Start () 
 	{
 		Cam =  GetComponent <Camera> ();
 	}
 	
-	// Update is called once per frame
+	// Обновление каждый фрейм
 	void Update () 
 	{
 		if (Input.GetKey(KeyCode.LeftArrow))
 			Cam.transform.Translate(Vector3.left * speed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.RightArrow))
 			Cam.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.UpArrow))
+			Cam.transform.Translate(Vector3.up * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.DownArrow))
+			Cam.transform.Translate (Vector3.down * speed * Time.deltaTime);
 	}
 }
--- Assets/Scripts/Move_like_jager.cs
+++ Assets/Scripts/Move_like_jager.cs
@@ -1,25 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Move_like_jager : MonoBehaviour 
 {
+	[SerializeField]
+	float rotation_speed = 200f;
 
 	NavMeshAgent agent;
-
+	private RaycastHit hit;
+	private Vector3 Look_target = new Vector3();
+	private Vector3 LastLook_target = new Vector3();
+	private Vector3 dir;
+	private float view_angle;
 	void Start() 
 	{
 		agent = GetComponent<NavMeshAgent>();
 	}
 
-	void Update() {
-		if (Input.GetMouseButtonDown(0)) 
+	void Update() 
+	{
+		if (Input.GetMouseButtonDown(1)) 
 		{
-			RaycastHit hit;
-
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) 
 			{
 				agent.destination = hit.point;
+				Look_target = hit.point;
 			}
 		}
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) 
+		{
+			Look_target = hit.point;
+		}
+		LookHere ();
 	}
+	void LookHere()
+	{
+		if (Look_target != LastLook_target)
+		{
+			CalculateAngle(Look_target);
+			if(view_angle > 0.1f)
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), rotation_speed * UnityEngine.Time.deltaTime);
+		}
+	}
+	private void CalculateAngle(Vector3 temp)
+	{
+		dir = new Vector3(temp.x, transform.position.y, temp.z) - transform.position;
+		view_angle = Vector3.Angle(dir, transform.forward);
+	}
+		
 }
