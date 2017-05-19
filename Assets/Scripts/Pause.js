06511e4f276393cb084b00553750506311039bf4
Real001 <evgenii_rorot@mail.ru>
2017-05-19 17:04:24 +0700

    Pause

---
+++ Assets/Scripts/Pause.js
@@ -0,0 +1,13 @@
+﻿var paused = false;
+
+function Update(){
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (!paused) {
+            Time.timeScale = 0;
+            paused = true;
+        } else {
+            Time.timeScale = 1;
+            paused = false;
+        }
+    }
+}
