377863f67c217bfb37c18130898872fb9977d4de
Real001 <evgenii_rorot@mail.ru>
2017-05-27 14:51:52 +0700

    online game

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisabled;
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+        {
+            for (int i =0; i<componentsToDisabled.Length; i++)
+            {
+                componentsToDisabled [i].enabled = false;
+            }
+        } 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
