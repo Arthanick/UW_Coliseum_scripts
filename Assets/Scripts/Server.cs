2446052a73f1dd10488863fd63d5d5d1201aa6f2
Real001 <evgenii_rorot@mail.ru>
2017-06-04 17:19:07 +0700

    server

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(NetworkView))]
+
+public class Server : MonoBehaviour {
+
+    private int playerCount = 0; // сколько игроков подключенно
+    public int PlayersCount
+    {
+        get { return playerCount; }
+    }
+
+    void OnServerInitialized()
+    {
+        SendMessage("SpawnPlayer", "PlayerServer");//создание локального игрока-сервера
+    }
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        playerCount++; // увеличение кол-во игроков
+        GetComponent<NetworkView>().RPC("SpawnPlayer", player, "Player " + playerCount.ToString());
+    }
+
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        playerCount--;
+        Network.RemoveRPCs(player); //очистка списка процедур игрока
+        Network.DestroyPlayerObjects(player);
+    }
+	
+}
