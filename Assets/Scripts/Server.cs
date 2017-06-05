810a6e70353dcdf809bc60cb5b292cef51e9e223
Arthanick <Spider-terr@mail.ru>
2017-06-05 15:41:18 +0700

    some networking fixes

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 public class Server : MonoBehaviour {
 
     private int playerCount = 0; // сколько игроков подключенно
+    private GameObject creation;
+
+    public GameObject net_player_pref;
     public int PlayersCount
     {
         get { return playerCount; }
@@ -14,13 +17,15 @@ public class Server : MonoBehaviour {
 
     void OnServerInitialized()
     {
+        Create_player();
         SendMessage("SpawnPlayer", "PlayerServer");//создание локального игрока-сервера
     }
 
     void OnPlayerConnected(NetworkPlayer player)
     {
         playerCount++; // увеличение кол-во игроков
-        GetComponent<NetworkView>().RPC("SpawnPlayer", player, "Player " + playerCount.ToString());
+        Create_player();
+        Debug.Log("Player " + playerCount + " connected from " + player.ipAddress + ":" + player.port);
     }
 
     void OnPlayerDisconnected(NetworkPlayer player)
@@ -29,5 +34,15 @@ public class Server : MonoBehaviour {
         Network.RemoveRPCs(player); //очистка списка процедур игрока
         Network.DestroyPlayerObjects(player);
     }
-	
+
+    void Create_player()
+    {
+        creation = Instantiate(net_player_pref, transform.position, Quaternion.identity) as GameObject;
+        //creation
+    }
+
+    void Start()
+    {
+        Create_player();
+    }
 }
